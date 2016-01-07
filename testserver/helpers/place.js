047e275aa77101503a10f71261ead196f05f54a1
Sourav Kodali <souravkodali@gmail.com>
2016-01-07 21:16:26 +0530

    used async waterfall

--- testserver/helpers/place.js
+++ testserver/helpers/place.js
@@ -1,5 +1,7 @@
-var mysql;
+var mysql, async, _;
 mysql = require('../core/mysql');
+async = require ('async');
+_ = require ('underscore');
 module.exports = {
        
            getplaces: function(incoming, callback){
@@ -15,18 +17,45 @@ module.exports = {
                     },
 
            getmenu: function(incoming, callback){
+                   // var validateincoming, getdatafrommysql;
+                    
+                    function validateincoming(callback){
+                    if(incoming && !incoming.placeid && !incoming.type){
+                        callback("Missing important data");
+                    } else {
+                        callback(null);
+                    }
+                    }
+
+                    function getdatafrommysql(callback){
                     var params, query;
                     query = "select * from menu where placeid = ? and type = ?";
-                    if(incoming != null) {
+                   // if(incoming != null) {
                        params = [incoming.placeid, incoming.type];
-                    } 
+                   // } 
                     mysql.query(query, params, function(err, menus){
                     if((err == null) && (menus != null)) {
-                    callback(JSON.stringify({status: "success", data: {menus:menus}}));
+                        callback(err, menus);
                     } else {
-                            callback(JSON.stringify({status: "failed","message": err}));
+                        callback(err);
                     }  
                     });
-                   }   
+                    }
+                    async.waterfall([validateincoming, getdatafrommysql],function(err, menus){
+                    if(err == null){
+                       callback(JSON.stringify({status: "success", data: {menus: menus}}));
+                       } else {
+                                callback(JSON.stringify({status: "failed", message: err}));
+                       }
+                    });
+                   },
+
+              setorder: function(incoming, callback){
+
+                        function setordermysql(callback){
+                        var params, query;
+                        query = "";
+                        }
+                    }   
                  };
 
